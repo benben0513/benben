36ee0301ad8884ce91426c74b421350a4974f49c
benben <2532724835@qq.com>
2020-10-19 17:35:48 +0800

    111

---
+++ js  新/4  Node/day 1 下载node 下载nodemon/node.js
@@ -0,0 +1,10 @@
+// 创建一个服务
+    // 引入一个http 包
+//  const http = require('http');
+
+//     // 创建一个端口号为3000的服务
+// http.createServer( (req,res) => {
+//     // 向前端返回的值
+//     res.end('hello word');
+// }).listen(3000)
+console.log('_____________________')
